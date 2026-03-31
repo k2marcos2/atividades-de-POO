86e1635e005bb10bbe9c267dc54276df12a75be7
k2marcos <marcosfeitosapalmas@gmail.com>
2026-03-31 10:32:57 -0300

    mensagem do commit

--- atividade_31_para_as_oras.java
+++ atividade_31_para_as_oras.java
@@ -1,5 +1,18 @@
 package sla;
+import java.util.Scanner;
 
 public class atividade_31_para_as_oras {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int mask = 0b1000000;
+		int n = sc.nextInt();
+		if ((n & mask) != 0) {
+		System.out.println("7th bit is true!");
+		}
+		else {
+		System.out.println("7th bit is false");
+		}
+		sc.close();
+		}
 }
